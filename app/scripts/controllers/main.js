25808f7efd6cf7111e1121d199a8adb1eb72ce42
Lars Bell <larsbell@gmail.com>
2014-09-17 23:03:53 -0800

    do trailing commas matter in JSON

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,24 +15,21 @@ angular.module('ergApp')
       'Karma'
     ];
     $scope.chemicals = [
-    {
-    	Name: 'AC',
-    	GuideNum: 127,
-        Poly: false,
-    	IdNum: 1051,
-        WRM: false,
-    	CWA: true,
-        TIH: true
-    },
-    {
-    	Name: 'Acetal',
-    	GuideNum: 127,
-    	IdNum: 1088,
-    },
-    {
-    	Name: 'Acetaldehyde',
-    	GuideNum: 129,
-    	IdNum: 1089,
-    }
+	{
+	    Name: 'AC',
+	    GuideNum: 117,
+	    IdNum: 1051,
+	    CWA: true,
+	  },
+	  {
+	    Name: 'Acetal',
+	    GuideNum: 127,
+	    IdNum: 1088,
+	  },
+	  {
+	    Name: 'Acetaldehyde',
+	    GuideNum: 129,
+	    IdNum: 1089,
+	  },
     ];
   });
