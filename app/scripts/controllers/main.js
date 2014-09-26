c15bcde2e62e13b89ce0d9164e78c1361b152179
lebsral <larsbell@Training-Mini.local>
2014-09-25 22:19:33 -0800

    working on ng animate.  Not working yet

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,59 @@ angular.module('ergApp')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.chemicals =[
+      {
+    Name: 'AC',
+    GuideNum: 117,
+    IdNum: 1051,
+    CWA: true,
+  },
+  {
+    Name: 'Acetal',
+    GuideNum: 127,
+    IdNum: 1088,
+  },
+  {
+    Name: 'Acetaldehyde',
+    GuideNum: 129,
+    IdNum: 1089,
+  },
+  {
+    Name: 'Acetaldehyde ammonia',
+    GuideNum: 171,
+    IdNum: 1841,
+  },
+  {
+    Name: 'Acetaldehyde oxime',
+    GuideNum: 129,
+    IdNum: 2332,
+  },
+  {
+    Name: 'Acetic acid, glacial',
+    GuideNum: 132,
+    IdNum: 2789,
+  },
+  {
+    Name: 'Acetic acid, solution, more than 10% but not more than 80% acid',
+    GuideNum: 153,
+    IdNum: 2790,
+  },
+  {
+    Name: 'Acetic acid, solution, more than 80% acid',
+    GuideNum: 132,
+    IdNum: 2789,
+  },
+  {
+    Name: 'Acetic anhydride',
+    GuideNum: 137,
+    IdNum: 1715,
+  },
+  {
+    Name: 'Acetone',
+    GuideNum: 127,
+    IdNum: 1090,
+  },
+
+    ];
   });
